405530c7221df68e756409c3762292362b96efc1
aellianti <anandya.ellianti@gmail.com>
2024-04-08 16:44:30 +0700

    Update test case assertion and button selector.

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -5,7 +5,7 @@ import selectors from "../support/selectors.js"
 
 describe('First test suite', () => {
 
-    beforeEach( 'open template search page', () => {
+    beforeEach(() => {
         cy.visit('/maker/mydesigns/logodrafts/8bf87f2e-0afd-4621-8bc0-ab2ac015dd97/templatetypes?searchTerm=tech%20challenge')
     })
 
@@ -28,14 +28,17 @@ describe('First test suite', () => {
         cy.get(selectors.resultPage).should('not.contain', 'Facebook Post')
 
         onSearchPage.selectACategory('Facebook')
-        cy.get(selectors.resultPage).should('contain', 'Facebook Post')
-        })
+        cy.get(selectors.resultPage).should('contain', 'Facebook Post')})
 
     it('should be able to load more design from search result page', () => {
+        let itemCount
+
         onSearchPage.searchTemplate('flowers')
+        cy.get('.masonry-item').its('length').then((length) => { itemCount = length })
         cy.get('button').contains('Load More Designs',{ matchCase: false })
             .should('be.visible')
             .click()
+        cy.get('.masonry-item').should(($items) => {expect($items.length).be.greaterThan(itemCount) })
     })
 
     it('should be able to select from search result', () => {
--- cypress/support/pageObjects/templateSearchPage.js
+++ cypress/support/pageObjects/templateSearchPage.js
@@ -7,8 +7,7 @@ export class TemplateSearchPage{
 
     searchTemplate(searchTerm){
         cy.get('input[id=searchTerm]').clear().type(searchTerm)
-        cy.get('[data-v-60789f99][class="tw-font-sans tw-cursor-pointer tw-border-2 tw-font-bold tw-text-white tw-uppercase tw-border-solid tw-transition-colors tw-duration-300 sm:tw-rounded-tl-none sm:tw-rounded-bl-none tw-w-full tw-text-sm tw-py-3 tw-px-4 tw-rounded tw-bg-primary-500 tw-border-primary-500 hover:tw-border-primary-700 hover:tw-bg-primary-600"]')
-        .click()
+        cy.get('button').contains('search').click()
     }
 
     selectACategory(category){ 
@@ -19,6 +18,8 @@ export class TemplateSearchPage{
         })
 
     }
+
+
     
 }
 
